f976170d7f1fd92c27fba414b4203bbdf691ce18
frankwswang <41162655+frankwswang@users.noreply.github.com>
2026-06-30 23:55:52 -0400

    Added docstrings for `PhaseFactor` and `evalPhase`.

--- src/Constant.jl
+++ src/Constant.jl
@@ -1,4 +1,4 @@
-export PauliSym, PauliX, PauliY, PauliZ, PhaseFactor
+export PauliSym, PauliX, PauliY, PauliZ, PhaseFactor, evalPhase
 
 const PauliXMatEntries = (0,   1,    1,  0) #> (m11, m21, m12, m22)
 const PauliYMatEntries = (0, 1im, -1im,  0)
@@ -31,12 +31,34 @@ end
 getPauliSym(num::Integer) = PauliSym(num)
 
 
+"""
+    PhaseFactor <: Enum{UInt8}
+
+A `UInt8`-backed enumeration of the four phase coefficients `im^k` that a Pauli-group
+element may carry (the fourth roots of unity). Each instance's integer value is the
+exponent `k` of `im`:
+
+    posRea::PhaseFactor  =>  im^0 # +1
+    posImg::PhaseFactor  =>  im^1 # +im
+    negRea::PhaseFactor  =>  im^2 # -1
+    negImg::PhaseFactor  =>  im^3 # -im
+
+Use [`evalPhase`](@ref) to obtain the value for the corresponding complex coefficient.
+"""
 @enum PhaseFactor::UInt8 begin
     posRea=0 #> i^0 == +1
     posImg=1 #> i^1 == +im
     negRea=2 #> i^2 == -1
     negImg=3 #> i^3 == -im
 end
+
+
+"""
+    evalPhase(phase::PhaseFactor) -> Complex{Int}
+
+Return the complex phase coefficient represented by `phase`.
+"""
+evalPhase(phase::PhaseFactor) = im^Int(phase)
 
 
 const CONSTVAR!!subscriptNum = 

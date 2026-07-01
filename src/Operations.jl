13b082021703befe6568f89450bda2753c629a20
frankwswang <41162655+frankwswang@users.noreply.github.com>
2026-07-01 02:35:54 -0400

    Added `mul` and `uml!` for `PauliStr`.

--- src/Operations.jl
+++ src/Operations.jl
@@ -1,5 +1,64 @@
 export checkCommute, checkAntiCom, evalCommute, evalAntiCom, getFrustrationGraph
 
+
+
+"""
+    mul(s1::PauliStr, s2::PauliStr) -> PauliStr
+
+Multiply two `PauliStr`, returning the product `s3::PauliStr` with the its associated phase 
+folded into `.phase`.
+"""
+function mul(s1::PauliStr, s2::PauliStr)
+    bl1 = iszero(s1.n)
+    if bl1 || iszero(s2.n) #> Trivial case: s * I == s, phases combined
+        controlStr = bl1 ? s2 : s1
+        x3 = controlStr.x
+        z3 = controlStr.z
+        n3 = controlStr.n 
+        phase = PhaseFactor((UInt8(s1.phase) + UInt8(s2.phase)) & 0x3)
+    else
+        x1, x2 = s1.x, s2.x
+        z1, z2 = s1.z, s2.z
+        n1, n2 = s1.n, s2.n
+        len1, len2 = length(z1), length(z2)
+        nWord, n3 = n1 <= n2 ? (len2, n2) : (len1, n1)
+        z3 = Memory{UInt}(undef, nWord)
+        x3 = Memory{UInt}(undef, nWord)
+        nY1 = nY2 = nY3 = 0
+        sgnParity = 0
+
+        @inbounds for w in 1:nWord
+            #> Pad shorter string with identities
+            z1w = w <= len1 ? z1[begin+w-1] : zero(UInt)
+            x1w = w <= len1 ? x1[begin+w-1] : zero(UInt)
+            z2w = w <= len2 ? z2[begin+w-1] : zero(UInt)
+            x2w = w <= len2 ? x2[begin+w-1] : zero(UInt)
+
+            x3w = x1w ⊻ x2w
+            z3w = z1w ⊻ z2w
+            x3[begin+w-1] = x3w
+            z3[begin+w-1] = z3w
+            nY1 += count_ones(z1[begin+w-1] & x1[begin+w-1])
+            nY2 += count_ones(z2[begin+w-1] & x2[begin+w-1])
+            nY3 += count_ones(z3w & x3w)
+            sgnParity += count_ones(x1[begin+w-1] & z2[begin+w-1])
+        end
+
+        phase = (( Int(UInt8(s1.phase)) + Int(UInt8(s2.phase))
+                   + 3*(nY1 + nY2) + nY3 + 2*(sgnParity & 1) ) & 3) |> UInt |> PhaseFactor
+    end
+
+    PauliStr(x3, z3, phase, n3)
+end
+
+
+function mul!(s::PauliStr, p::PhaseFactor)
+    newPhase = PhaseFactor((UInt8(s.phase) + UInt8(p)) & 3)
+    s.phase = newPhase
+    s
+end
+
+
 function checkCommute(str1::PauliStr, str2::PauliStr)::Bool
     z1, x1 = str1.z, str1.x
     z2, x2 = str2.z, str2.x
